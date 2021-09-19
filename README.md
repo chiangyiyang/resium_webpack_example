66013d3c98c5082d315630e7145e69e2864ced07
chiangyiyang <chiangyiyang@gmail.com>
2021-09-19 15:29:08 +0800

    初始專案

---
+++ README.md
@@ -0,0 +1,10 @@
+# 使用Webpack的Resium專案範例
+
+## 複製Resium所有的檔案，再將其載入HTML中
+
+### 參考資料:
+
+* [CesiumJS](https://cesium.com/cesiumjs/)  
+* [Resium](https://resium.reearth.io/)  
+* [Webpack](https://webpack.js.org/)  
+* [Resium Example Webpack](https://github.com/reearth/resium/tree/main/example/webpackp)  
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const path = require("path");
+
+const webpack = require("webpack");
+const HtmlPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlTagsPlugin = require("html-webpack-tags-plugin");
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = (_env, args) => ({
+  externals: {
+    cesium: "Cesium",
+  },
+  mode: args.mode === "production" ? "production" : "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: args.mode === "production" ? [] : ["react-refresh/babel"],
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      CESIUM_BASE_URL: JSON.stringify("/cesium"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "node_modules/cesium/Build/Cesium",
+          to: "cesium",
+        },
+      ],
+    }),
+    new HtmlPlugin({
+      template: "index.html"
+    }),
+    new HtmlTagsPlugin({
+      append: false,
+      tags: ["cesium/Widgets/widgets.css", "cesium/Cesium.js"],
+    }),
+    ...(args.mode === "production" ? [] : [new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()]),
+  ],
+});
